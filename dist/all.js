4657578d939250c9f4ac9708de927df978ba98da
Matheus Garcia <matheusgarciadg@gmail.com>
2017-05-04 01:35:29 -0300

    criado a pagina curriculo, adicionado um arquivo js para os efeitos scroll, adicionado cdn para jquery ui

--- dist/all.js
+++ dist/all.js
@@ -1,4 +1,33 @@
+$(function () {
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 600) {
+            $('#to-the-top').fadeIn(100);
+        } else {
+            $('#to-the-top').fadeOut(0);
+        }
+    });
+    $('#to-the-top').click(function () {
+        $("html, body").animate({
+            scrollTop: 0
+        }, {
+            duration: 2000,
+            easing: "easeOutQuint"
+        });
+    });
 
+
+    $('.mouse').click(function () {
+        $("html, body").animate({
+            scrollTop: 650
+        }, {
+            duration: 2000,
+            easing: "easeOutQuint"
+        });
+    });
+
+
+
+});
 // Avoid `console` errors in browsers that lack a console.
 (function() {
     var method;
